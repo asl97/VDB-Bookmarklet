6a0ee3b3da4556e53fb1e7cc39bab0048a8297bf
asl97 <asl97@airmail.cc>
2019-05-17 23:50:44 +0800

    Add multi-page support and concurrent limit (e621)

--- src/e621.js
+++ src/e621.js
@@ -75,32 +75,86 @@ bookmarklet([
 
         let proxy = 'https://cors-anywhere.herokuapp.com/';
         let id = window.location.href.split('/').pop();
-        fetch(`https://e621.net/pool/show.json?id=${id}`).then((r)=>{
-            r.json().then((json)=>{
-                var zip = new JSZip();
-                for (post of json.posts){
-                    let filename = post['file_url'].split('/').pop();
-                    zip.file(filename, urlToPromise(proxy+post['file_url']), {binary:true});
-                    if (post['has_notes']){
-                        zip.file(filename+".xml", urlToPromise(`https://e621.net/note/index.xml?post_id=${post["id"]}`), {binary:true});
-                    }
+        let links = [];
+
+        async function get_page(page){
+            let r = await fetch(`https://e621.net/pool/show.json?id=${id}&page=${page}`);
+            let json = await r.json();
+            return json
+        }
+
+        function process_page(json){
+            for (post of json.posts){
+                let filename = post['file_url'].split('/').pop();
+                links.push([filename, proxy+post['file_url']])
+                if (post['has_notes']){
+                    links.push([filename+".xml", `https://e621.net/note/index.xml?post_id=${post["id"]}`])
                 }
+            }
+        }
 
-                // When everything has been downloaded, we can trigger the dl
-                zip.generateAsync({type:"blob"}, function updateCallback(metadata) {
-                    var msg = "progression : " + metadata.percent.toFixed(2) + " %";
-                    console.log(msg);
-                }).then(function callback(blob) {
-                    filename = `${json.name}-${id}.zip`;
-                    // see FileSaver.js
-                    saveAs(blob, filename);
-                    console.log("done !");
-                }, function (e) {
-                    alert(e);
-                });
+        async function urlWithNameToPromise(filename, url){
+            return [filename, await urlToPromise(url)]
+        }
 
-            })
-        })
+        // limit concurrent async
+        async function process_urls(urls, limit=4) {
+            let executing = [];
+            let promises = [];
+
+            // limit the amount of concurrent requests
+            for (let [filename, url] of urls) {
+                if (executing.length >= limit){
+                    await Promise.race(executing);
+                }
+                let promise = urlWithNameToPromise(filename, url);
+                executing.push(promise);
+                promises.push(promise);
+                promise.then(()=>{executing.splice(executing.indexOf(promise), 1)})
+            };
+
+            return Promise.all(promises)
+        }
+
+        async function download_images(archive_name){
+            var zip = new JSZip();
+
+            let datas = await process_urls(links);
+            for (let [filename, data] of datas){
+                zip.file(filename, data, {binary:true});
+            }
+
+            // When everything has been downloaded, we can trigger the dl
+            zip.generateAsync({type:"blob"}, function updateCallback(metadata) {
+                var msg = "progression : " + metadata.percent.toFixed(2) + " %";
+                console.log(msg);
+            }).then(function callback(blob) {
+                // see FileSaver.js
+                saveAs(blob, archive_name);
+                console.log("done !");
+            }, function (e) {
+                alert(e);
+            });
+        }
+
+        async function start(){
+            let first_page = await get_page(1);
+            process_page(first_page);
+            let total_count = first_page.post_count;
+            let count = first_page.posts.length;
+            let page_num = 2;
+            
+            while (count < total_count){
+                let json = await get_page(page_num);
+                process_page(json)
+                count += json.posts.length;
+                page_num+=1
+            }
+
+            download_images(`${first_page.name}-${id}.zip`)
+        }
+
+        start()
     },
     // Require running in non-isolated scope due to CORS
     local_scope
